adc16d91122122b9634687e7d015603dd0a1dc77
siva626203 <sivakumar.626203@gmail.com>
2023-12-29 21:02:07 +0530

    route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { Route,Routes } from 'react-router-dom';
+import Home from './pages/Home';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Final Year Project</h1>
-      </header>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Home(props) {
+    return (
+        <div>
+            <h1 className='m-5 text-red-900'>Home</h1>
+        </div>
+    );
+}
+
+export default Home;
